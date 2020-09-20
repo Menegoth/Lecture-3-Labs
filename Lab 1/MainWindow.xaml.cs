61fb7b98fd4fdfb1b39f60f7e635b3c0a0cef388
Antonio <antonioareas@me.com>
2020-09-20 12:22:22 -0700

    Add project files.

---
+++ Lab 1/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/// Week 3		Lab 1
+/// File Name: MainWindow.xaml.cs
+/// @author: Antonio Monteiro
+/// Date:  September 14th 2020
+
+namespace Lab_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            //decalare variables
+            string firstName = fnTextBox.Text;
+            string lastName = lnTextBox.Text;
+
+            printPigLatinName(firstName, lastName);
+
+        }
+
+        static void printPigLatinName(string first, string last)
+        {
+
+            //declare string variables
+            string pigLatin = "ay";
+            string firstLetter;
+            string fullName;
+
+            //set both to lowercase
+            first = first.ToLower();
+            last = last.ToLower();
+
+            //fix first name
+            //start with getting firstletter
+            firstLetter = first.Substring(0, 1);
+
+            //remove first letter from name and add it to the end
+            first = first.Substring(1);
+            first = first + firstLetter + pigLatin;
+
+            //capitalize it
+            first = first.Substring(0, 1).ToUpper() + first.Substring(1);
+
+            //do the same for last name
+            //start with getting firstletter
+            firstLetter = last.Substring(0, 1);
+
+            //remove first letter from name and add it to the end
+            last = last.Substring(1);
+            last = last + firstLetter + pigLatin;
+
+            //capitalize it
+            last = last.Substring(0, 1).ToUpper() + last.Substring(1);
+
+            //combine both names
+            fullName = first + " " + last;
+            MessageBox.Show(fullName);
+
+        }
+
+    }
+}
---
+++ Lab 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/// Week 3		Lab 2
+/// File Name: Program.cs
+/// @author: Antonio Monteiro
+/// Date:  September 20th 2020
+
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //declare lamp variables
+            bool pluggedIn, burnedOut;
+
+            pluggedIn = true;
+            burnedOut = true;
+
+            //call function
+            checkLamp(pluggedIn, burnedOut);
+
+        }
+
+        static void checkLamp(bool pluggedIn, bool burnedOut)
+        {
+
+            //check lamp conditions
+            if (!pluggedIn)
+            {
+                Console.WriteLine("You need to plug in your lamp.");
+            } else if (!burnedOut)
+            {
+                Console.WriteLine("You need to replace the bulb.");
+            } else
+            {
+                Console.WriteLine("Take your lamp in for repair.");
+            }
+
+        }
+
+    }
+}
